fc873e920400597c0da0a93e4ccbc8327ca99044
Joshua-Carbajal <78939092+Joshua-Carbajal@users.noreply.github.com>
2021-02-15 08:25:11 -0800

    Add files via upload
    Short program that asks the user to input the following information:
    First Name, Last Name, Street Address, City, State, Zip and prints the 
    information in a certain format to the user.

---
+++ carbajal_joshua_AS02.py
@@ -0,0 +1,21 @@
+# File Name: carbajal_joshua_AS02.py
+# File Path: /home/carbaja1/Python/carbajal_joshua_AS02.py
+# Run Commmand: sudo python3 /home/carbaja1/Python/carbajal_joshua_AS02.py
+
+# Joshua Carbajal
+# Date (09/30/19)
+# AS.02
+# Input and Printing
+
+firstName = input("Please enter your first name: ")
+lastName = input("Please enter your last name: ")
+address = input("Enter your street address: ")
+city = input("Enter your city: ")
+state = input("Enter your state: ")
+zipCode = input("Enter your zip code: ")
+
+print("\n")
+print(firstName, lastName)
+print(address)
+print(city + "," + state, zipCode + "\n")
+
